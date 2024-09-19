0a3fc5440f113f49b3cf1af5827e285588de0d98
Kks562 <kavin.krishnas.562@gmail.com>
2024-09-19 13:43:12 +0530

    add font awesome in vitejs

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@fortawesome/fontawesome-svg-core',
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/react-fontawesome'
+      ],
+    },
+  },
 });
